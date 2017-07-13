829102f01918f3e0c0e2ccecdbe02afb9caeeead
Jane Jeon <JaneJeon9719@gmail.com>
2017-07-13 11:27:19 -0700

    converted indents

--- download.php
+++ download.php
@@ -17,19 +17,19 @@ curl_close($ch);
 $dom = new DOMDocument();
 @$dom->loadHTML($html);
 foreach($dom->getElementsByTagName('a') as $node) {
-	$link = substr($node->getAttribute('href'), 2);
-	# only interested in .bz2 files (the comment archive for each month)
-	if (preg_match('/\.bz2$/', $link) && !in_array($link, $downloadList)
-			&& (strcmp($link, end($downloadList)) > 0))
-		$downloadList[] = $link;
+    $link = substr($node->getAttribute('href'), 2);
+    # only interested in .bz2 files (the comment archive for each month)
+    if (preg_match('/\.bz2$/', $link) && !in_array($link, $downloadList)
+            && (strcmp($link, end($downloadList)) > 0))
+        $downloadList[] = $link;
 }
 
 # use curl to directly save each month's archive into file to save memory
 foreach($downloadList as $file) {
-	@$fp = fopen($localDirectory.$file, 'wb');
-	$ch = curl_init($serverDirectory.$file);
-	curl_setopt($ch, CURLOPT_FILE, $fp);
-	curl_exec($ch);
-	curl_close($ch);
-	fclose($fp);
+    @$fp = fopen($localDirectory.$file, 'wb');
+    $ch = curl_init($serverDirectory.$file);
+    curl_setopt($ch, CURLOPT_FILE, $fp);
+    curl_exec($ch);
+    curl_close($ch);
+    fclose($fp);
 }
--- tags.php
+++ tags.php
@@ -10,24 +10,24 @@ $total = 0;
 
 # read through every file to check for tags
 while ($file = $dir->read()) {
-	if (!preg_match('/\.(\S)*$/', $file)) {
-		@$fp = fopen($localDirectory.$file, 'rb');
-		while (!feof($fp)) {
+    if (!preg_match('/\.(\S)*$/', $file)) {
+        @$fp = fopen($localDirectory.$file, 'rb');
+        while (!feof($fp)) {
             @$comment = json_decode(fgets($fp), true);
             # some lines are not in proper json format, so skip those
             if (!is_array($comment)) continue;
-			foreach ($comment as $tag => $value)
-				array_key_exists($tag, $tags) ? $tags[$tag]++ : $tags[$tag] = 1;
-			$total++;
-		}
-		fclose($fp);
-	}
+            foreach ($comment as $tag => $value)
+                array_key_exists($tag, $tags) ? $tags[$tag]++ : $tags[$tag] = 1;
+            $total++;
+        }
+        fclose($fp);
+    }
 }
 
 $dir->close();
 
 # convert tally to percentage
 foreach ($tags as $tag => $tally)
-	$tags[$tag] = sprintf('%.2f%%', 100.0*$tally/$total);
+    $tags[$tag] = sprintf('%.2f%%', 100.0*$tally/$total);
 
 print_r($tags);
